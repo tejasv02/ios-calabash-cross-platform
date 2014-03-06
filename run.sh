d872e80e1dd110ebfb11ce33ee98ce72db9efcb8
Tejasvi Manmatha <Tejasvi.Manmatha@akqa.com>
2014-03-06 20:46:32 +0000

    updating run.sh file

--- run.sh
+++ run.sh
@@ -1,40 +1,29 @@
-#!/bin/sh
-clear
-DATE=`date +%d-%m-%Y-%H-%M`
+echo "Choose scheme name and update it in SCHEME_XC"
+echo "click manage scheme and choose scheme name and find the build configuration BUILD_CONFIG" 
+echo "if your features(automation test features) are present in different folder rather than folder where project is present , mention the path in FEATURES_PATH
+or else remove the argument."
+echo "APPNAME - name of the application usually same as the scheme name"
+echo "PROJ_LOC - location of xcode project if thats same as current folder remove this option."
 
-if [ "$#" != "2" ]; then
-	echo "\n\n\n2 ARGUMENTS NEEDED"
-	echo "1) clean(clean project) or NA (for running project without cleaning"
-	echo "2) Tags selected for test run ex: @sanity or @reg"
-	echo "\nsample command: sh run.sh clean @sanity\n"
-	exit
-fi
 
-if [ -z "$2" ] ; then
-echo "Tags not specified using @failed"
-tagged_test=@failed
-else
-tagged_test=$2
-fi
-
-PROJ_NAME="MJTransitionEffect"
 SCHEME_XC="MJTransitionEffect-cal"
 BUILD_CONFIG="Debug"
+FEATURES_PATH="features"
 ARCHITECTURE_SELECTED=i386
 APPNAME="MJTransitionEffect"
 PROJ_LOC="../MJTransitionEffect/MJTransitionEffect.xcodeproj"
 
 
-if [ "$1" == "clean" ] ; then
-echo "cleaning and building"
-killall Xcode
-xcodebuild  -scheme "${SCHEME_XC}" -project "${PROJ_LOC}" -configuration Debug ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator build
+if [ $1 = "clean" ]
+then
+xcodebuild  -scheme "${SCHEME_XC}" -project "${PROJ_LOC}" -configuration Debug ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator clean
 fi
+
+xcodebuild  -scheme "${SCHEME_XC}" -project "${PROJ_LOC}" -configuration Debug ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator build
 
 BUILT_PRODUCTS_DIR=$(xcodebuild -project "${PROJ_LOC}" ARCHS="${ARCHITECTURE_SELECTED}" ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator  -configuration "${BUILD_CONFIG}" -showBuildSettings | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*" | xargs -L1 dirname)
 
 APP_BUNDLE_PATH_VAR="${BUILT_PRODUCTS_DIR}"/"${BUILD_CONFIG}"-iphonesimulator/"${APPNAME}".app
 echo $APP_BUNDLE_PATH_VAR
 
-echo BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p ios features/  --tag $tagged_test
-BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p ios features/  --tag $tagged_test
+BUNDLE_ID=$BUNDLE DEVICE=iphone APP_BUNDLE_PATH="${APP_BUNDLE_PATH_VAR}" bundle exec cucumber -p ios features/  --tag $2
